5393ba83b661f40c0cb68773568e612c6f5120e7
MattDecanted <trish@smidgewines.com>
2025-09-04 16:11:57 +0930

    Create BrandButton.tsx

---
+++ src/components/ui/BrandButton.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+import { Button } from "@/components/ui/button";
+
+type ButtonProps = React.ComponentProps<typeof Button>;
+
+/** Brand-coloured primary button that still accepts all shadcn Button props */
+const BrandButton = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, ...props }, ref) => {
+    const base = "bg-brand-orange text-white shadow hover:opacity-95 [&_svg]:text-white";
+    return <Button ref={ref} className={[base, className].filter(Boolean).join(" ")} {...props} />;
+  }
+);
+BrandButton.displayName = "BrandButton";
+
+export default BrandButton;
