08de4ae4fa516cb3de3f0f9403c21605d0076249
MattDecanted <trish@smidgewines.com>
2025-09-08 10:03:25 +0930

    Create routes.ts

---
+++ src/routes.ts
@@ -0,0 +1,18 @@
+// src/routes.ts
+export const ROUTES = {
+  home: "/",
+  challenges: "/challenges",
+  shorts: "/shorts",
+  modules: "/modules",
+  vocab: "/vocab",
+  dailyQuiz: "/daily-quiz",
+  wineOptions: "/wine-options",
+  wineOptionsMultiplayer: "/wine-options/multiplayer",
+  guessWhat: "/games/guess-what",
+  leaderboard: "/swirdle/leaderboard",
+  events: "/events",
+  community: "/community",
+  login: "/login",
+  dashboard: "/dashboard",
+  account: "/account",
+};
